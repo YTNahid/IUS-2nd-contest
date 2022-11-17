68636202013953d042ea2c93329d507616ac4f50
YTNahid <65461710+YTNahid@users.noreply.github.com>
2022-11-18 02:12:59 +0600

    Update G.c

--- G.c
+++ G.c
@@ -1,24 +1,23 @@
-// https://www.codechef.com/problems/TWOSTR
+// https://codeforces.com/problemset/problem/798/A
 #include <stdio.h>
+#include <string.h>
 
-int main(void) {
+int main() {
 
-    int t, i;
-    scanf("%d",&t);
+    int i, length, count = 0;
+    char s[20];
+
+    gets(s);
     
-    for (i = 0; i < t; i++) {
-        int count = 0, j;
-        char x[10], y[10];
-        scanf("%s%s", &x, &y);
-        for (j = 0; x[j] != '\0'; j++) {
-            if (x[j] == y[j] || x[j] == '?' || y[j] == '?') count = 1;
-            else {
-                count = 0;
-                break;
-            }
-        }
-        if (count) printf("YES\n");
-        else printf("NO\n");
-    }
+    length = strlen(s);
 
+    for(i = 0; i < length/2; i++) {
+        if(s[i] != s[length-i-1]) {
+            count++;
+        }
+    }
+    if (count == 1 || length == 1) printf("YES");
+    else if (count == 0 && length%2 == 1) printf("YES");
+    else printf("NO");
+    
 }
